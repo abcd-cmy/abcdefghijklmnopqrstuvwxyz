32c9db46865471d1d01947f6fe9a43bf99e4bbcc
abcd-cmy <104753748+abcd-cmy@users.noreply.github.com>
2022-05-02 12:18:28 +0530

    Create tokenbucket.py

---
+++ tokenbucket.py
@@ -0,0 +1,15 @@
+def tokenbucket():
+    n = int(input('enter number of packets to be sent :'))
+    ch = 1
+    x = 0
+    m = 0
+    a = []
+    while (n>=x):
+        ch = int(input('enter 1 to send the packet :'))
+        if (ch == 1):
+            print("f'{x+1}th packet is sent")
+        else:
+            print('packet is not sent')
+    print(f{x}, 'packets are not sent')
+    print('f{m}packets are sent')
+tokenbucket()
